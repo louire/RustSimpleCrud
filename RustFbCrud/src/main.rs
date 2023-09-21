4447a03a6862b814125392a555d790ae2e711266
Loui Recio <lreciopersonal@gmail.com>
2023-09-20 23:10:31 -0500

    i need to sleep, THREAD MAIN PANICKET AT CALLET RESULTUNWRAP COULD WAIT, and i had 6 percent battery on my mac

--- RustFbCrud/src/main.rs
+++ RustFbCrud/src/main.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
     name: String,
-    age: u8,
+    age: u32,
     email: String,
 }
 
@@ -20,11 +20,11 @@ struct Response {
 async fn main(){
     let user = User{
         name: "Loui Recio".to_string(),
-        age: 23,
-        email: "lreciopersonal@gmail.com".to_string()
+        age:23,
+        email: "lreciopersonal@gmail.com".to_string(),
     };
 
-    let firebase = Firebase::new("https://rustcrudbor-default-rtdb.firebaseio.com/").unwrap();
+    let firebase = Firebase::new("https://fbrustcrud-default-rtdb.firebaseio.com/").unwrap();
 
     let response = set_user(&firebase, &user).await;
 
@@ -44,18 +44,42 @@ async fn main(){
 }
 
 async fn set_user(firebase_client: &Firebase, user: &User) -> Response{
+    let firebase = firebase_client.at("users");
+    let _users = firebase.set::<User>(&user).await;
+    return string_to_response(&_users.unwrap().data);
 }
 
 async fn get_users(firebase_client: &Firebase) -> HashMap<String, User>{
+    let firebase = firebase_client.at("users");
+    let users = firebase.get::<HashMap<String, User>>().await;
+    println!("{:?}", users);
+    return users.unwrap();
 }
 
 
 async fn get_user(firebase_client: &Firebase, id: &String) -> User{
+    let firebase = firebase_client.at("users").at(&id);
+    let user = firebase.get::<User>().await;
+    return user.unwrap();
 }
 
 async fn update_user(firebase_client: &Firebase, id: &String, user: &User) -> User{
+    let firebase = firebase_client.at("users").at(&id);
+    let _user = firebase.update::<User>(&user).await;
+    return string_to_user(&_user.unwrap().data);
 }
 
 async fn delete_user(firebase_client: &Firebase, id: &String){
+    let firebase = firebase_client.at("users").at(&id);
+    let _result = firebase.delete().await;
 }
 
+// convert a string to a response
+fn string_to_response(s: &str) -> Response{
+    serde_json::from_str(s).unwrap()
+}
+
+//convert a string to a user
+fn string_to_user(s: &str) -> User{
+    serde_json::from_str(s).unwrap()
+}
