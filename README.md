e748841de7d670f570cbdc1835531a79a50b8675
Loui Recio <lreciopersonal@gmail.com>
2023-09-20 22:23:23 -0500

    doing well but have to fix some bugs, feeling sleepy

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Arrow in the knee
 A very simple crud on a very simple file, all create, read, update and delete are in main.rs
 im just practicing soo that
 im using firebase btw
+a simple crud but without api, i am the api
+
+i also disabled copilot, i hate copilot when im using rust
+
+Documentation:
+https://tokio.rs/tokio/tutorial/hello-tokio
--- RustFbCrud/src/main.rs
+++ RustFbCrud/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use firebase_rs::Firebase;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    name: String,
+    age: u8,
+    email: String,
 }
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    name: String,
+}
+
+
+#[tokio::main]
+async fn main(){
+    let user = User{
+        name: "Loui Recio".to_string(),
+        age: 23,
+        email: "lreciopersonal@gmail.com".to_string()
+    };
+
+    let firebase = Firebase::new("https://rustcrudbor-default-rtdb.firebaseio.com/").unwrap();
+}
+    
